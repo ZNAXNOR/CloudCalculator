fae5fa8b7acc27cf6a0bb0792463212ba639304c
ZNAXNOR <omkarsdalvi1@gmail.com>
2024-12-12 12:44:00 +0530

    Creates out file during Serve

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
+import fs from "fs";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Set this to enable static export
+  async generateStaticProps() {
+    const filePath = path.join(process.cwd(), "public", "mockPricing.json");
+    if (fs.existsSync(filePath)) {
+      const pricingData = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+      return { props: { pricingData } };
+    }
+    return { props: { pricingData: {} } };
+  },
 };
 
 export default nextConfig;
